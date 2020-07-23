f5bf69e2979c190cb1565ed1be87db6e0ebf842a
18044928 <18044928@myrp.edu.sg>
2020-07-23 12:35:30 +0800

    finalpscommit

--- app/src/main/java/com/myapplicationdev/android/p10_knowyourfacts/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p10_knowyourfacts/MainActivity.java
@@ -1,14 +1,99 @@
 package com.myapplicationdev.android.p10_knowyourfacts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int reqCode = 12345;
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+    Button btnRead;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vPager = findViewById(R.id.viewpager1);
+        btnRead = findViewById(R.id.btnRead);
+
+
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.MINUTE, 5);
+
+                Intent intent = new Intent(MainActivity.this,
+                        ScheduledNotificationReceiever.class);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        MainActivity.this, reqCode,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+            }
+        });
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.next) {
+            int max = vPager.getChildCount();
+            if (vPager.getCurrentItem() < max-1){
+                int nextPage = vPager.getCurrentItem() + 1;
+                vPager.setCurrentItem(nextPage,true);
+            }
+            return true;
+        } else if (id == R.id.previous){
+            int previousPage = vPager.getCurrentItem() - 1;
+            vPager.setCurrentItem(previousPage, true);
+        }else{
+            Random rnd = new Random();
+            vPager.setCurrentItem(rnd.nextInt(2));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
